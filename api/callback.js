5e68c28b3baec91a30361bd7bf68ccee4289ae5d
Khusrav-afk <155685012+Khusrav-afk@users.noreply.github.com>
2026-04-25 19:45:01 +0500

    Update callback.js

--- api/callback.js
+++ api/callback.js
@@ -39,7 +39,7 @@ export default async function handler(req, res) {
           'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-          from: 'SolarMiner <noreply@bpserv.host>',
+          from: 'SolarMiner <onboarding@resend.dev>',
           to: ['Mmmagicmkr@gmail.com'],
           subject: `✅ Оплата подтверждена! Заказ ${orderId}`,
           html: `
